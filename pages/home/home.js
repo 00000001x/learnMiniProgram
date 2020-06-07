20c6dce949250616a2920f80c5e767d077a2875d
yuanfang <yuanfang@qq.com>
2020-06-07 20:08:51 +0800

    获取用户信息

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,71 @@
 // pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+//getApp()获取App()产生的实例对象
+const app = getApp()
+console.log(app.globalData.name)
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    
+  },
+  handleUserInfo:function(info){
+    console.log(info)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面初次渲染完成
    */
-  methods: {
+  onReady: function () {
+    
+  },
 
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
