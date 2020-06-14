d430c51e8c55ed9e1957747bf09e0c66b746dc52
yuanfang <yuanfang@qq.com>
2020-06-14 12:33:42 +0800

    09_绑定事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,65 @@
 // pages/home/home.js
-Component({
+Page({
   /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    titles:['上衣','裤子','鞋子']
+  },
+  handleClick(){
+    console.log('-------点击了')
+  },
+  handleTouchStart(){
+    console.log('-------handleTouchStart')
+  },
+  handleTouchEnd(){
+    console.log('-------handleTouchEnd')
+  },
+  handleMove(){
+    console.log('-------handleMove')
+  },
+  handleLongpress(){
+    console.log('-------handleLongpress')
+  },
+  handletap(){
+    console.log('-------handletap')
+  },
+  handleObject(event){
+    console.log(event)
+  },
+  handleEventEnd(event){
+    console.log(event)
+  },
+  handleOuter(event){
+    console.log('-----outer-------',event)
+  },
+  handleInner(event){
+    console.log('-----inner-------',event)
+  },
+  handleEventParam(event){
+    const dataset = event.currentTarget.dataset;
+    const index = dataset.index;
+    const title = dataset.currentitem;
+    console.log(index+'--------------'+title)
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  //事件冒泡和事件捕获
+  handleCaptureView1(event){
+    console.log('-----事件捕获view1')
+  },
+  handleBindView1(event){
+    console.log('-----事件冒泡view1')
+  },
+  handleCaptureView2(event){
+    console.log('-----事件捕获view2')
+  },
+  handleBindView2(event){
+    console.log('-----事件冒泡view2')
+  },
+  handleCaptureView3(event){
+    console.log('-----事件捕获view3')
+  },
+  handleBindView3(event){
+    console.log('-----事件冒泡view3')
   }
 })
