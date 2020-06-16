2b8737fe296542ff6fab8595dde9e27667c8aa7e
yuanfang <yuanfang@qq.com>
2020-06-16 20:28:27 +0800

    11_网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+//导入封装的request方法  (import相对路径)
+import request from '../../serivce/network.js'
 Page({
 
   /**
@@ -12,55 +14,59 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      //发送网络请求
+      //1.使用原生的请求方式
+        this.get_data_origin()
+      //2.使用封装的request工具方式
+      //Promise最大的好处就是防止回调地狱
+       request({
+        method:'POST',
+        //参数
+        data:{
+          name:'sky',
+          age:'16'
+        },
+       }).then(res=>{
+        console.log(res)
+       }).catch(err=>{
+         console.log(err)
+       })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin:function(){
+      //1.1 发送最简单的get请求
+      wx.request({
+        url: 'http://123.207.32.32:8000/recommend',
+        success:function(res){
+          console.log(res)
+      }
+      })
+      // 1.2 发送get请求传递参数
+      wx.request({
+        url: 'http://123.207.32.32:8000/recommend',
+        data:{
+          type:'aa',
+          page:1
+        },
+        success:function(res){
+          console.log(res)
+      }
+      })
+      //1.3 发送post请求并携带参数
+      wx.request({
+        url: 'http://httpbin.org/post',
+        method:'POST',
+        //参数
+        data:{
+          name:'sky',
+          age:'16'
+        },
+        success:function(res){
+            console.log(res)
+        },
+        fail:function(err){
+          console.log(err)
+        }
+      })
   }
 })
