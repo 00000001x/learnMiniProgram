a7fe0e163f17f39dcbb441bc7a8ea8c570124a07
yuanfang <yuanfang@qq.com>
2020-06-17 10:02:08 +0800

    1212_弹出框+分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,67 @@
 // pages/home/home.js
 Page({
+  handleShowToast(){
+    wx.showToast({
+      title: '你好啊',
+    }),
+    wx.showToast({
+      title: '加载中',
+      duration: 3000,
+      icon:'loading',
+      mask:true,
+      success:function(){
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+      },
+      fail:function(){
 
+      },
+      complete:function(){
+
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title:'确认提交吗?',
+      content:'lalala',
+      success:function(res){
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载中',
+      mask:true
+    }),
+    setTimeout(()=>{
+      //必须手动调用才会消失
+      wx.hideLoading({
+        complete: (res) => {
+        }
+      })
+    },2000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowActionSheet(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      success:function(res){
+        console.log(res.tapIndex)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
+   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return{
+      title:'你好啊',
+      path:'/pages/about/about',//点击进去后的路径，默认是首页
+      imageUrl:'' //默认是当页面的截图,可自定义图片 本地、网络都可以
+    }
   }
 })
